8a6a4bfbf753e4b0e4026e1f2befe7003b6bf94d
Eric Blois <ericblois291@gmail.com>
2022-07-10 10:24:42 -0400

    Create offer table

---
+++ knex/migrations/20220710142032_create_offer_table.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    return knex.schema
+    .createTable('Offer', table => {
+        table.uuid('offerID').primary().notNullable().defaultTo(knex.raw('uuid_generate_v4()'))
+        table.uuid('fromID').notNullable()
+        table.uuid('toID').notNullable()
+        table.specificType('fromItems', 'uuid ARRAY')
+        table.specificType('toItems', 'uuid ARRAY')
+        table.decimal('fromPayment', 7, 2)
+        table.decimal('toPayment', 7, 2)
+        table.text('deliveryMethod').notNullable()
+        table.text('responseType')
+        table.bigInteger('responseTime')
+    })
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTable('Offer')
+  };
+  
