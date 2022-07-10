cd95765dbd87251f8ea28b69ce4de9c783d7b30e
Eric Blois <ericblois291@gmail.com>
2022-07-10 10:36:35 -0400

    fixes

--- knex/migrations/20220710134134_create_exchange_table.js
+++ knex/migrations/20220710134134_create_exchange_table.js
@@ -7,6 +7,9 @@ exports.up = function(knex) {
   .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
   .createTable('Exchange', table => {
     table.uuid('exchangeID').primary().notNullable().defaultTo(knex.raw('uuid_generate_v4()'))
+    table.uuid('userID1').notNullable()
+    table.uuid('userID2').notNullable()
+    table.uuid('offerID').notNullable()
     table.geography('deliveryGeoCoords')
     table.text('deliveryAddress')
     table.bigInteger('deliveryPlannedTime')
